9408b9d0afbd8969528e8d01b24f10ab7a248dd2
Birk Karlsen-Bæck <birk.karlsen.baeck@gmail.com>
2023-06-09 19:01:36 +0200

    changed a few parser descriptions and changed parameter scan for otfb.

--- lxplus_setup/parsers.py
+++ lxplus_setup/parsers.py
@@ -141,7 +141,7 @@ def lhc_llrf_argument_parser(add_help=False):
 
     # Parsers for the LHC cavity loop
     parser.add_argument('~~analog_gain', '~ga', type=float, default=6.79e-6,
-                        help='Analog gain in the LHC RFFB; default is 6.79e-6 * 1.3 A/V')
+                        help='Analog gain in the LHC RFFB; default is 6.79e-6 A/V')
     parser.add_argument('~~analog_delay', '~ta', type=float, default=170e-6,
                         help='Analog feedback delay in the LHC RFFB; default is 170e-6 s')
     parser.add_argument('~~digital_gain', '~gd', type=float, default=10,
@@ -149,13 +149,13 @@ def lhc_llrf_argument_parser(add_help=False):
     parser.add_argument('~~digital_delay', '~td', type=float, default=400e-6,
                         help='Digital feedback delay in the LHC RFFB; default is 400e-6 s')
     parser.add_argument('~~loop_delay', '~tl', type=float, default=650e-9,
-                        help='Total loop delay in the LHC RFFB; default is 650e-9 * 1.05 s')
+                        help='Total loop delay in the LHC RFFB; default is 650e-9 s')
     parser.add_argument('~~loaded_q', '~ql', type=float, default=20000,
                         help='Loaded quality in the LHC cavity; default is 20000')
     parser.add_argument('~~comb_alpha', '~ca', type=float, default=15 / 16,
                         help='Comb filter coefficient for the LHC OTFB; default is 15/16')
     parser.add_argument('~~otfb_delay', '~to', type=float, default=1.2e-6,
-                        help='Complementary delay in the LHC OTFB; default is  1.2e-6 * 0.95 s')
+                        help='Complementary delay in the LHC OTFB; default is  1.2e-6 s')
     parser.add_argument('~~otfb_gain', '~go', type=float, default=10,
                         help='The OTFB gain; default is 7.5')
     parser.add_argument('~~detuning_mu', '~dl', type=float, default=0,
